9a026f78b26cb73fdad912708d2b495f8bb41185
tiantiandas <tiantiandas@outlook.com>
2022-02-13 19:14:17 +0800

    Add scripts

---
+++ scripts/initialize.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+
+sudo apt update
+
+sudo apt install -y --no-install-recommends \
+ca-certificates curl gnupg lsb-release apt-transport-https
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt update
+sudo apt install -y  --no-install-recommends docker-ce kubelet kubeadm kubectl
+
+sudo apt-mark hold kubectl kubelet kubeadm docker-ce
+sudo systemctl enable kubelet docker
